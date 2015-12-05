9a53c43e4f3e59100202877a4f7409cdbc0fb7a4
Rob Wells <rob@robjwells.com>
2015-12-05 19:46:56 +0000

    Add JavaScript solutions for days 1 and 2

---
+++ day1.js
@@ -0,0 +1,33 @@
+#!/usr/local/bin/node
+
+'use strict';
+
+var fs = require('fs');
+
+let instructions = fs.readFileSync('day1_input.txt', 'utf-8').split(''),
+    instruction_values = {'(': 1, ')': -1};
+
+var final_floor = instructions
+    .map(function (char) {
+        return instruction_values[char];
+    })
+    .reduce(function (prev, current) {
+        return prev + current;
+    });
+
+console.log('Final floor:', final_floor);
+
+
+// Part two
+
+var floor = 0,
+    basement_instruction = 1;
+
+for (let char of instructions) {
+    floor += instruction_values[char];
+    basement_instruction += 1;
+    if (floor === -1) {
+        console.log('First basement instruction:', basement_instruction)
+        break;
+    }
+};
---
+++ day2.js
@@ -0,0 +1,55 @@
+#!/usr/local/bin/node
+
+'use strict';
+
+var fs = require('fs');
+
+let instructions = fs.readFileSync('day2_input.txt', 'utf-8')
+    .trimRight()
+    .split('\n')
+    .map(function (el) {
+        return el
+            .split('x')
+            .map(function (el) {
+                return parseInt(el, 10);
+            });
+    });
+
+var surface_area = function (l, w, h) {
+    return (2 * l * w) + (2 * w * h) + (2 * h * l);
+};
+var smallest_side = function (l, w, h) {
+    return Math.min.apply(Math, [(l * w), (w * h), (h * l)]);
+};
+
+
+// Part one
+
+var wrapping_paper = 0;
+for (let dimensions of instructions) {
+    wrapping_paper += surface_area.apply(this, dimensions);
+    wrapping_paper += smallest_side.apply(this, dimensions);
+}
+
+console.log('wrapping paper:', wrapping_paper);
+
+// Part two
+
+var ribbon = 0;
+for (let dimensions of instructions) {
+    var dimensions_copy = dimensions.slice();  // JS only sorts in place
+    dimensions_copy.sort(function (a, b) {
+        // JS compares numbers by their string representation by default.
+        // You have to provide your own comparison function to compare
+        // numbers as numbers. JavaScript is mad.
+        return a - b;
+    })
+
+    var s1 = dimensions_copy[0],
+        s2 = dimensions_copy[1],
+        s3 = dimensions_copy[2];
+    ribbon += (2 * s1) + (2 * s2);
+    ribbon += s1 * s2 * s3;
+}
+
+console.log('ribbon:', ribbon);
