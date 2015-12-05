729e1fb3ddc262c013b7a1f06b0a678337cf365e
Rob Wells <rob@robjwells.com>
2015-12-05 16:11:17 +0000

    improve day2

--- day2.py
+++ day2.py
@@ -1,7 +1,8 @@
 #!/usr/local/bin/python3
 
 with open('day2_input.txt') as f:
-    instructions = f.read().rstrip()
+    instructions = [[int(i) for i in line.split('x')]
+                    for line in f.read().splitlines()]
 
 
 def surface_area(l, w, h):
@@ -12,19 +13,20 @@ def smallest_side(l, w, h):
     return min([(l * w), (w * h), (h * l)])
 
 
-total = 0
+wrapping_paper = 0
 
-for line in instructions.splitlines():
-    l, w, h = map(int, line.split('x'))
-    total += surface_area(l, w, h)
-    total += smallest_side(l, w, h)
+for l, w, h in instructions:
+    wrapping_paper += surface_area(l, w, h)
+    wrapping_paper += smallest_side(l, w, h)  # slack
 else:
-    print('wrapping paper:', total)
+    print('wrapping paper:', wrapping_paper)
+
+
+# Part two
 
 ribbon = 0
 
-for line in instructions.splitlines():
-    l, w, h = map(int, line.split('x'))
+for l, w, h in instructions:
     s1, s2, s3 = sorted([l, w, h])
     ribbon += 2 * s1 + 2 * s2
     ribbon += l * w * h  # bow
