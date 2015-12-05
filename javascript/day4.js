f514dc22a13b15be0ac55b6fbab1248ae312c458
Rob Wells <rob@robjwells.com>
2015-12-05 23:49:26 +0000

    add day4.js

---
+++ javascript/day4.js
@@ -0,0 +1,26 @@
+#!/usr/local/bin/node
+
+'use strict';
+
+var crypto = require('crypto')
+
+
+var md5_digest = function (string) {
+    var hash = crypto.createHash('md5');
+    hash.update(string, 'utf-8')
+    return hash.digest('hex')
+};
+
+var md5_prefix = 'ckczppom';
+['00000', '000000'].forEach(function (prefix) {
+   var num = 1;
+   while (true) {
+       var digest = md5_digest(md5_prefix + String(num));
+       if (digest.startsWith(prefix)) {
+           console.log(num, digest);
+           break;
+       } else {
+           num += 1;
+       }
+   }
+});
