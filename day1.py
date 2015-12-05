36a8004d18e60de8555f8e5430e52986ec388c4b
Rob Wells <rob@robjwells.com>
2015-12-05 15:50:54 +0000

    improve day1.py

--- day1.py
+++ day1.py
@@ -3,27 +3,17 @@
 with open('day1_input.txt') as f:
     instructions = f.read().rstrip()
 
-floor = 0
+instruction_values = {'(': 1, ')': -1}
 
-for char in instructions:
-    if char == '(':
-        floor += 1
-    else:
-        floor -= 1
-
-print('final floor:', floor)
+print('final floor:', sum(instruction_values[char] for char in instructions))
 
 
 # Part two
 
-
 floor = 0
 
 for idx, char in enumerate(instructions, start=1):
-    if char == '(':
-        floor += 1
-    else:
-        floor -= 1
+    floor += instruction_values[char]
     if floor == -1:
         print('first basement instruction:', idx)
         break
