afa0e353a0f3d33434d5dc89be42eba53d31821a
Rob Wells <rob@robjwells.com>
2015-12-05 19:53:10 +0000

    day2.js: appease the jslint gods

--- day2.js
+++ day2.js
@@ -4,7 +4,7 @@
 
 var fs = require('fs');
 
-let instructions = fs.readFileSync('day2_input.txt', 'utf-8')
+var instructions = fs.readFileSync('day2_input.txt', 'utf-8')
     .trimRight()
     .split('\n')
     .map(function (el) {
@@ -26,30 +26,30 @@ var smallest_side = function (l, w, h) {
 // Part one
 
 var wrapping_paper = 0;
-for (let dimensions of instructions) {
-    wrapping_paper += surface_area.apply(this, dimensions);
-    wrapping_paper += smallest_side.apply(this, dimensions);
-}
+instructions.forEach(function (dimensions) {
+    wrapping_paper += surface_area.apply(null, dimensions);
+    wrapping_paper += smallest_side.apply(null, dimensions);
+});
 
 console.log('wrapping paper:', wrapping_paper);
 
 // Part two
 
 var ribbon = 0;
-for (let dimensions of instructions) {
+instructions.forEach(function (dimensions) {
     var dimensions_copy = dimensions.slice();  // JS only sorts in place
     dimensions_copy.sort(function (a, b) {
         // JS compares numbers by their string representation by default.
         // You have to provide your own comparison function to compare
         // numbers as numbers. JavaScript is mad.
         return a - b;
-    })
+    });
 
     var s1 = dimensions_copy[0],
         s2 = dimensions_copy[1],
         s3 = dimensions_copy[2];
     ribbon += (2 * s1) + (2 * s2);
     ribbon += s1 * s2 * s3;
-}
+});
 
 console.log('ribbon:', ribbon);
