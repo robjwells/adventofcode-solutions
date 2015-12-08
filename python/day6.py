bbdf6bf41e48f1cd878ea9125b62c9f565a76f16
Rob Wells <rob@robjwells.com>
2015-12-08 21:39:23 +0000

    day6.py: clean up more comments

--- python/day6.py
+++ python/day6.py
@@ -4,8 +4,6 @@ with open('../day6_input.txt') as f:
     lines = [line.rsplit(maxsplit=3) for line in f.read().splitlines()]
 
 # Part one
-
-# Part one
 # lights = [[False for i in range(1000)] for j in range(1000)]
 
 # Part two
@@ -16,14 +14,14 @@ for instruction, start, _, end in lines:
     end_x, end_y = [int(part) for part in end.split(',')]
 
     if instruction == 'turn on':
-#         func = lambda state: True       # Part one
-        func = lambda state: state + 1  # Part two
+        # func = lambda state: True         # Part one
+        func = lambda state: state + 1      # Part two
     elif instruction == 'turn off':
-#         func = lambda state: False    # Part one
+        # func = lambda state: False        # Part one
         func = lambda state: state if state == 0 else state - 1  # Part two
     elif instruction == 'toggle':
-#         func = lambda state: not state  # Part one
-        func = lambda state: state + 2  # Part two
+        # func = lambda state: not state    # Part one
+        func = lambda state: state + 2      # Part two
 
     for col in range(start_x, end_x + 1):
         row_part = slice(start_y, end_y + 1)
