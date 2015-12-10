0716a1f9245472f1d3ee653411f5f52b8f60fd63
Rob Wells <rob@robjwells.com>
2015-12-10 21:25:07 +0000

    day8.py: clean up, use safer ast.literal_eval

--- python/day8.py
+++ python/day8.py
@@ -1,5 +1,6 @@
 #!/usr/local/bin/python3
 
+from ast import literal_eval    # safer than plain eval
 import re
 
 with open('../day8_input.txt') as f:
@@ -7,9 +8,11 @@ with open('../day8_input.txt') as f:
 
 # Part one
 total_symbols = sum(len(s) for s in strings)
-total_chars = sum(len(eval(s)) for s in strings)
-print(total_symbols - total_chars)
+total_chars = sum(len(literal_eval(s)) for s in strings)
+print('Part one:', total_symbols - total_chars)
 
 # Part two
-newly_encoded_chars = sum(len(re.escape(s)) + 2 for s in strings)
-print(newly_encoded_chars - total_symbols)
+newly_encoded_chars = sum(
+    len(re.escape(s)) + 2   # Add 2 for the (missing) surrounding quotes
+    for s in strings)
+print('Part two:', newly_encoded_chars - total_symbols)
