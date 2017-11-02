77adaae6e1b539a4e5ed4c5356aa738a469e4724
Rob Wells <rob@robjwells.com>
2017-11-02 13:11:19 +0000

    Implement search, few fixes and reporting

--- python/day9.py
+++ python/day9.py
@@ -2,6 +2,7 @@
 
 from collections import defaultdict, deque, namedtuple
 import pathlib
+import sys
 
 input_file = pathlib.Path(__file__).parent.parent.joinpath('day9_input.txt')
 
@@ -64,11 +65,13 @@ class Graph:
 
     def weight(self, src: str, dst: str):
         """Return the weight between two nodes if edge exists"""
-        return self.connections[src].get(dst).weight
+        for e in self.connections[src]:
+            if e.dst == dst:
+                return e.weight
 
     def edges(self, node: str):
         """Return all edges for node"""
-        return self.connections[node].values()
+        return self.connections[node]
 
 
 def create_graph(parsed_lines):
@@ -79,7 +82,48 @@ def create_graph(parsed_lines):
     return graph
 
 
+def breadth_first_search(graph: Graph, start: str):
+    """Search the graph for the shortest path that vistis all nodes
+
+    Returns the shortest path (a list of strings) and the weight (int)
+    """
+    def total_weight(path):
+        pairs = zip(path, path[1:])
+        weight = sum(graph.weight(*p) for p in pairs)
+        return weight
+
+    queue = deque([[start]])
+    all_nodes = graph.connections.keys()
+
+    shortest_path = None
+    lowest_weight = sys.maxsize
+
+    while queue:
+        temp_path = queue.pop()
+        if total_weight(temp_path) >= lowest_weight:
+            continue
+        if all_nodes - set(temp_path):
+            # Not visited all nodes yet
+            for edge in graph.edges(temp_path[0]):
+                if edge.dst not in temp_path:
+                    new_path = temp_path + [edge.dst]
+                    queue.append(new_path)
+        else:
+            # Visited all nodes
+            temp_weight = total_weight(temp_path)
+            if temp_weight < lowest_weight:
+                lowest_weight = temp_weight
+                shortest_path = temp_path
+
+    return shortest_path, lowest_weight
+
+
 if __name__ == '__main__':
     with open(input_file) as f:
         lines = parse_input(f.read())
     graph = create_graph(lines)
+    start_node = list(graph.connections.keys())[0]
+    path, weight = breadth_first_search(graph, start_node)
+
+    print(f'Shortest path is:\n{path}')
+    print(f'Weight is {weight}')
