7068258e692872b2fb7bbdade55dadaecff2ffb5
Rob Wells <rob@robjwells.com>
2017-11-02 16:48:06 +0000

    Add Day 11 python file

---
+++ python/day11.py
@@ -0,0 +1,14 @@
+#!/usr/local/bin/python3
+
+
+puzzle_input = 'vzbxkghb'
+
+
+def validate_password(password):
+    pass
+
+
+def test_validate_password():
+    assert not validate_password('hijklmmn')
+    assert not validate_password('abbceffg')
+    assert not validate_password('abbcegjk')
