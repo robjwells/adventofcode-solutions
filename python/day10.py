130de07688be39ff74a3f5656adc8145419faa3f
Rob Wells <rob@robjwells.com>
2017-11-02 16:22:47 +0000

    Day 10: Add python solution for first part

---
+++ python/day10.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python3
+
+puzzle_input = 1113122113
+
+def parse_input(sequence):
+    char, *text = str(sequence)
+    count = 1
+    output = ''
+    for new_char in text:
+        if new_char == char:
+            count += 1
+        else:
+            output += str(count) + char
+            char = new_char
+            count = 1
+    else:
+        output += str(count) + char
+    return output
+
+def test_parse_input():
+    assert parse_input('211') == '1221'
+
+if __name__ == '__main__':
+    for x in range(40):
+        puzzle_input = parse_input(puzzle_input)
+    print(len(puzzle_input))
