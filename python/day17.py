67ceca9a68124d4c83c4ecbe36dc6ce12ccb6948
Rob Wells <rob@robjwells.com>
2017-11-04 22:27:56 +0000

    Day 17 solution in python

---
+++ python/day17.py
@@ -0,0 +1,56 @@
+#!/usr/bin/env python3
+
+from itertools import combinations
+
+puzzle_input = [43, 3, 4, 10, 21, 44, 4, 6, 47, 41, 34,
+                17, 17, 44, 36, 31, 46, 9, 27, 38]
+puzzle_input.sort()
+
+
+def brute_force(containers, amount):
+    eggnog_combos = [
+        c
+        for i in range(len(containers))
+        for c in combinations(containers, i)
+        if sum(c) == amount]
+    number_combos = len(eggnog_combos)
+    eggnog_combos.sort(key=len)
+    min_containers = len(eggnog_combos[0])
+    min_containers_ways = len(
+        [c for c in eggnog_combos if len(c) == min_containers])
+    return (number_combos, min_containers_ways)
+
+
+def find_containers(containers, amount):
+    if not amount:
+        yield []
+    elif containers and min(containers) > amount:
+        # This is just an early return for lists of containers that
+        # can’t possibly work.
+        return
+    else:
+        for idx, container in enumerate(containers):
+            if container <= amount:
+                for others in find_containers(
+                        containers[idx+1:], amount - container):
+                    yield [container] + others
+
+
+# This is much faster than brute forcing, about 3 times
+def recursive(containers, amount):
+    containers.sort()
+    matching_containers = list(find_containers(containers, amount))
+
+    # Part one
+    number_combos = len(matching_containers)
+
+    # Part two
+    matching_containers.sort(key=len)
+    min_containers = len(matching_containers[0])
+    min_ways = len(
+        [c for c in matching_containers if len(c) == min_containers])
+    return (number_combos, min_ways)
+
+
+# print(brute_force(puzzle_input, 150))
+print(recursive(puzzle_input, 150))
