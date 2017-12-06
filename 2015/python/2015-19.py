b587f95617c57584de2967486a5d0efe0fedb560
Rob Wells <rob@robjwells.com>
2017-12-06 22:18:37 +0000

    2015-19 Python: Add test for second part

--- 2015/python/2015-19.py
+++ 2015/python/2015-19.py
@@ -5,6 +5,7 @@ import re
 
 input_file = pathlib.Path('../input/2015-19.txt')
 
+
 def parse_input(text: str) -> (list, str):
     """Return a list of replacement pairs and the molecule string"""
     replacement_block, molecule = text.rstrip().split('\n\n')
@@ -34,6 +35,11 @@ def generate_replacements(molecule: str, replacements: list) -> set:
     return generated
 
 
+def steps_to_molecule(molecule: str, replacements: list):
+    """Return the minimum number of replacements needed to make molecule"""
+    pass
+
+
 def test_replacements():
     test_molecule = 'HOH'
     test_replacements = [
@@ -45,6 +51,21 @@ def test_replacements():
                                    replacements=test_replacements)
     expected = {'HOOH', 'HOHO', 'OHOH', 'HHHH'}
     assert result == expected
+
+
+def test_steps_to_molecule():
+    test_molecule = 'HOH'
+    test_replacements = [
+        ('e', 'H'),
+        ('e', 'O'),
+        ('H', 'HO'),
+        ('H', 'OH'),
+        ('O', 'HH'),
+        ]
+    expected_steps = 6
+    result = steps_to_molecule(molecule=test_molecule,
+                               replacements=test_replacements)
+    assert result == expected_steps
 
 
 def main():
