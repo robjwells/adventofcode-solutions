8837b06f8e4b7f8e4f708e0b577e830d175ac6eb
Rob Wells <rob@robjwells.com>
2018-01-05 21:56:55 +0000

    2015-01.py: Refactor, add tests

--- 2015/python/2015-01.py
+++ 2015/python/2015-01.py
@@ -1,19 +1,68 @@
 #!/usr/local/bin/python3
 
-with open('../input/2015-01.txt') as f:
-    instructions = f.read().rstrip()
-
-instruction_values = {'(': 1, ')': -1}
-
-print('final floor:', sum(instruction_values[char] for char in instructions))
+import pathlib
 
 
-# Part two
+def final_floor(instructions):
+    """Calculate the final floor for a given set of instructions
 
-floor = 0
+    Instructions are a list of parentheses:
+        (   Move up a floor
+        )   Move down a floor
+    """
+    instruction_values = {'(': 1, ')': -1}
+    result = sum(instruction_values[char] for char in instructions)
+    return result
 
-for idx, char in enumerate(instructions, start=1):
-    floor += instruction_values[char]
-    if floor == -1:
-        print('first basement instruction:', idx)
-        break
+
+def first_basement_char(instructions):
+    """Return position of char that puts Santa in the basement
+
+    Positions start from 1.
+    """
+    instruction_values = {'(': 1, ')': -1}
+    floor = 0
+    for position, char in enumerate(instructions, start=1):
+        floor += instruction_values[char]
+        if floor == -1:
+            return position
+
+
+def main(puzzle_input):
+    p1_final_floor = final_floor(puzzle_input)
+    print(f'Part one, final floor: {p1_final_floor}')
+    basement_instruction = first_basement_char(puzzle_input)
+    print(f'Part two, first basement instruction: {basement_instruction}')
+
+
+def test_final_floor():
+    """final_floor calculates correct final floor from instructions"""
+    test_cases = [
+        ['(())', 0],
+        ['()()', 0],
+        ['(((', 3],
+        ['(()(()(', 3],
+        ['))(((((', 3],
+        ['())', -1],
+        ['))(', -1],
+        [')))', -3],
+        [')())())', -3],
+        ]
+    for instructions, expected in test_cases:
+        assert final_floor(instructions) == expected
+
+
+def test_first_basement_level():
+    """first_basement_char returns position of first sub-zero instruction"""
+    test_cases = [
+        [')', 1],
+        ['()())', 5]
+        ]
+    for instructions, expected in test_cases:
+        assert first_basement_char(instructions) == expected
+
+
+if __name__ == '__main__':
+    input_file = pathlib.Path('../input/2015-01.txt')
+    puzzle_input = input_file.read_text().rstrip()
+    main(puzzle_input)
