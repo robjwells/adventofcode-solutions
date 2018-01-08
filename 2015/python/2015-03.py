8ad3d29336e74b39d2daac5a6c8f6b50b1efa9b7
Rob Wells <rob@robjwells.com>
2018-01-08 21:31:44 +0000

    2015-03.py: Refactor into main function

--- 2015/python/2015-03.py
+++ 2015/python/2015-03.py
@@ -1,44 +1,45 @@
 #!/usr/local/bin/python3
 
 from collections import namedtuple
-
-with open('../input/2015-03.txt') as f:
-    instructions = f.read().rstrip()
-
-Point = namedtuple('Point', ['x', 'y'])
-location = Point(0, 0)
-visited = {location}
+import pathlib
 
 
-def new_loc(current_loc, instruction):
-    new_loc_table = {
-        '^': (current_loc.x, current_loc.y + 1),
-        'v': (current_loc.x, current_loc.y - 1),
-        '>': (current_loc.x + 1, current_loc.y),
-        '<': (current_loc.x - 1, current_loc.y)
-        }
-    return Point(*new_loc_table[instruction])
+def main(puzzle_input):
+    Point = namedtuple('Point', ['x', 'y'])
+    location = Point(0, 0)
+    visited = {location}
+
+    def new_loc(current_loc, instruction):
+        new_loc_table = {
+            '^': (current_loc.x, current_loc.y + 1),
+            'v': (current_loc.x, current_loc.y - 1),
+            '>': (current_loc.x + 1, current_loc.y),
+            '<': (current_loc.x - 1, current_loc.y)
+            }
+        return Point(*new_loc_table[instruction])
+
+    for char in puzzle_input:
+        location = new_loc(location, char)
+        visited.add(location)
+
+    print('At least one present:', len(visited))
+
+    # Part two
+    santa_loc = Point(0, 0)
+    robo_loc = Point(0, 0)
+    visited = {santa_loc}
+
+    for idx, char in enumerate(puzzle_input):
+        if idx % 2 == 0:  # Santa
+            santa_loc = new_loc(santa_loc, char)
+            visited.add(santa_loc)
+        else:  # robot
+            robo_loc = new_loc(robo_loc, char)
+            visited.add(robo_loc)
+
+    print('At least one present with santa and robot:', len(visited))
 
 
-for char in instructions:
-    location = new_loc(location, char)
-    visited.add(location)
-
-print('At least one present:', len(visited))
-
-
-# Part two
-
-santa_loc = Point(0, 0)
-robo_loc = Point(0, 0)
-visited = {santa_loc}
-
-for idx, char in enumerate(instructions):
-    if idx % 2 == 0:  # Santa
-        santa_loc = new_loc(santa_loc, char)
-        visited.add(santa_loc)
-    else:  # robot
-        robo_loc = new_loc(robo_loc, char)
-        visited.add(robo_loc)
-
-print('At least one present with santa and robot:', len(visited))
+if __name__ == '__main__':
+    puzzle_input = pathlib.Path('../input/2015-03.txt').read_text().rstrip()
+    main(puzzle_input)
