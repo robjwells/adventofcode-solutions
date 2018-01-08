a63f4550e2c6ba03d377d0b5cba98d847704d9b6
Rob Wells <rob@robjwells.com>
2018-01-08 22:20:10 +0000

    2015-03.py: Rewrite main to use new functions

--- 2015/python/2015-03.py
+++ 2015/python/2015-03.py
@@ -1,43 +1,6 @@
 #!/usr/local/bin/python3
 
-from collections import namedtuple
 import pathlib
-
-
-def main(puzzle_input):
-    Point = namedtuple('Point', ['x', 'y'])
-    location = Point(0, 0)
-    visited = {location}
-
-    def new_loc(current_loc, instruction):
-        new_loc_table = {
-            '^': (current_loc.x, current_loc.y + 1),
-            'v': (current_loc.x, current_loc.y - 1),
-            '>': (current_loc.x + 1, current_loc.y),
-            '<': (current_loc.x - 1, current_loc.y)
-            }
-        return Point(*new_loc_table[instruction])
-
-    for char in puzzle_input:
-        location = new_loc(location, char)
-        visited.add(location)
-
-    print('At least one present:', len(visited))
-
-    # Part two
-    santa_loc = Point(0, 0)
-    robo_loc = Point(0, 0)
-    visited = {santa_loc}
-
-    for idx, char in enumerate(puzzle_input):
-        if idx % 2 == 0:  # Santa
-            santa_loc = new_loc(santa_loc, char)
-            visited.add(santa_loc)
-        else:  # robot
-            robo_loc = new_loc(robo_loc, char)
-            visited.add(robo_loc)
-
-    print('At least one present with santa and robot:', len(visited))
 
 
 def move_location(current_location, direction):
@@ -117,6 +80,16 @@ def test_houses_visited_by_two_santas():
         ('^v^v^v^v^v', 11)]
     for instructions, total_houses in test_cases:
         assert houses_visited(instructions, number_of_santas=2) == total_houses
+
+
+def main(puzzle_input):
+    # Part one, houses visited by Santa himself
+    one_santa = houses_visited(puzzle_input)
+    print(f'Part one, single santa: {one_santa}')
+
+    # Part two
+    two_santas = houses_visited(puzzle_input, number_of_santas=2)
+    print(f'Part two, two santas: {two_santas}')
 
 
 if __name__ == '__main__':
