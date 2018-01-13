3dd107b852d2cf5482be1dd0585b8a8e472da543
Rob Wells <rob@robjwells.com>
2018-01-13 21:08:49 +0000

    2015-06.py: Wrap in if __main__ block

--- 2015/python/2015-06.py
+++ 2015/python/2015-06.py
@@ -1,27 +1,29 @@
 #!/usr/local/bin/python3
 
-with open('../input/2015-06.txt') as f:
-    lines = [line.rsplit(maxsplit=3) for line in f.read().splitlines()]
 
-lights = [[False for i in range(1000)] for j in range(1000)]
+if __name__ == '__main__':
+    with open('../input/2015-06.txt') as f:
+        puzzle_input = [line.rsplit(maxsplit=3)
+                        for line in f.read().splitlines()]
 
-for instruction, start, _, end in lines:
-    start_x, start_y = [int(part) for part in start.split(',')]
-    end_x, end_y = [int(part) for part in end.split(',')]
+    lights = [[False for i in range(1000)] for j in range(1000)]
 
-    if instruction == 'turn on':
-        # func = lambda state: True         # Part one
-        func = lambda state: state + 1      # Part two
-    elif instruction == 'turn off':
-        # func = lambda state: False        # Part one
-        func = lambda state: state - 1 if state else 0   # Part two
-    elif instruction == 'toggle':
-        # func = lambda state: not state    # Part one
-        func = lambda state: state + 2      # Part two
+    for instruction, start, _, end in puzzle_input:
+        start_x, start_y = [int(part) for part in start.split(',')]
+        end_x, end_y = [int(part) for part in end.split(',')]
 
-    for col in range(start_x, end_x + 1):
-        row_part = slice(start_y, end_y + 1)
-        lights[col][row_part] = map(func, lights[col][row_part])
+        if instruction == 'turn on':
+            # func = lambda state: True         # Part one
+            func = lambda state: state + 1      # Part two
+        elif instruction == 'turn off':
+            # func = lambda state: False        # Part one
+            func = lambda state: state - 1 if state else 0   # Part two
+        elif instruction == 'toggle':
+            # func = lambda state: not state    # Part one
+            func = lambda state: state + 2      # Part two
 
+        for col in range(start_x, end_x + 1):
+            row_part = slice(start_y, end_y + 1)
+            lights[col][row_part] = map(func, lights[col][row_part])
 
-print(sum(sum(col) for col in lights))
+    print(sum(sum(col) for col in lights))
