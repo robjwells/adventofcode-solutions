7dc195018fdeb306219848f35db05c58c473c6dd
Rob Wells <rob@robjwells.com>
2018-02-22 14:17:02 +0000

    2015-21.py: Add stub

---
+++ 2015/python/2015-21.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Advent of Code 2015, Day 21: RPG Simulator 20XX"""
+
+
+def main(puzzle_input):
+    pass
+
+
+if __name__ == '__main__':
+    # Puzzle input (boss stats):
+    #     Hit Points: 104
+    #     Damage: 8
+    #     Armor: 1
+    main()
