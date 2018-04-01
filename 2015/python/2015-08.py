0edb1bdabb7236b571b970fdff5cc741368b4c73
Rob Wells <rob@robjwells.com>
2018-04-01 19:06:46 +0100

    2015-08.py: Switch over two new-style part two for extra chars

--- 2015/python/2015-08.py
+++ 2015/python/2015-08.py
@@ -71,11 +71,7 @@ def main(puzzle_input):
     print('Part one:', total_symbols - parsed_total)
 
     # Part two
-    # Instead of actually escaping the string using re.escape and measuring it,
-    # you can work out the number of extra characters needed by counting the
-    # characters that would need escaping. Add 2 for the extra surrounding
-    # quote marks mentioned in the problem description.
-    total_extra_chars = sum(s.count('"') + s.count('\\') + 2 for s in strings)
+    total_extra_chars = sum(extra_chars_to_encode(s) for s in strings)
     print('Part two:', total_extra_chars)
 
 
