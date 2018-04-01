bc08195f774ed6750a7b6d815b0984efd49b05f2
Rob Wells <rob@robjwells.com>
2018-04-01 19:03:24 +0100

    2015-08.py: For posterity, fix the 2 -> 1 swap in old version
    I must have done a replace somehow, but I’ve no idea how or why.
    It was pestering me, while doing the test for the new-style part
    two (which passes fine), why you would only add 1. Just a bug!

--- 2015/python/2015-08.py
+++ 2015/python/2015-08.py
@@ -73,9 +73,9 @@ def main(puzzle_input):
     # Part two
     # Instead of actually escaping the string using re.escape and measuring it,
     # you can work out the number of extra characters needed by counting the
-    # characters that would need escaping. Add 1 for the extra surrounding
+    # characters that would need escaping. Add 2 for the extra surrounding
     # quote marks mentioned in the problem description.
-    total_extra_chars = sum(s.count('"') + s.count('\\') + 1 for s in strings)
+    total_extra_chars = sum(s.count('"') + s.count('\\') + 2 for s in strings)
     print('Part two:', total_extra_chars)
 
 
