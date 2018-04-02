991ab9330d5b44a2baa1b7bc28ff42b90441af56
Rob Wells <rob@robjwells.com>
2018-04-02 20:29:11 +0100

    2015-10.py: Revert to using strings for speed
    So… having to convert numbers that are up to 5.1 million digits
    long to and from strings is time-consuming. Who knew?

--- 2015/python/2015-10.py
+++ 2015/python/2015-10.py
@@ -3,11 +3,11 @@
 import pytest
 
 
-puzzle_input = 1113122113
+puzzle_input = '1113122113'
 
 
-def parse_input(number):
-    char, *text = str(number)
+def parse_number_string(number_string):
+    char, *text = str(number_string)    # str just in case
     count = 1
     output = ''
     for new_char in text:
@@ -20,24 +20,24 @@ def parse_input(number):
     else:
         # This is needed to ensure final digit is accounted for
         output += str(count) + char
-    return int(output)
+    return output
 
 
 @pytest.mark.parametrize('start,finish', [
-    (1, 11),
-    (11, 21),
-    (21, 1211),
-    (211, 1221),
-    (1211, 111221),
-    (111221, 312211),
+    ('1', '11'),
+    ('11', '21'),
+    ('21', '1211'),
+    ('211', '1221'),
+    ('1211', '111221'),
+    ('111221', '312211'),
     ])
-def test_parse_input(start, finish):
-    assert parse_input(start) == finish
+def test_parse_number_string(start, finish):
+    assert parse_number_string(start) == finish
 
 
 if __name__ == '__main__':
     for x in range(50):
         if x == 40:
             print(len(puzzle_input))
-        puzzle_input = parse_input(puzzle_input)
+        puzzle_input = parse_number_string(puzzle_input)
     print(len(puzzle_input))
