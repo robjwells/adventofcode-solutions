ee24b30a98a5ac75af0562a8e35c0375a1f8cadd
Rob Wells <rob@robjwells.com>
2018-04-02 20:52:44 +0100

    2017-01.py: Casually fix that test that I just broke

--- 2017/python/2017-01.py
+++ 2017/python/2017-01.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import pytest
+
 
 def circ_pairs(sequence):
     """Zip consequtive elements in sequence and wrap round at the end
@@ -44,18 +46,26 @@ def sum_matching_digits(pairs):
     return sum(a for a, b in pairs)
 
 
-def main(puzzle_input):
-    seq = int_to_sequence(puzzle_input)
+def sum_matching_neighbours(circular_number):
+    """Sums all digits that match the next digit in the sequence"""
+    seq = int_to_sequence(circular_number)
     pairs = circ_pairs(seq)
     matching = matching_pairs(pairs)
-    print('Part one:', sum_matching_digits(matching))
+    return sum_matching_digits(matching)
 
 
-def test_full():
-    assert main(1122) == 3
-    assert main(1111) == 4
-    assert main(1234) == 0
-    assert main(91212129) == 9
+def main(puzzle_input):
+    print('Part one:', sum_matching_neighbours(puzzle_input))
+
+
+@pytest.mark.parametrize('circ_number,total', [
+    (1122, 3),
+    (1111, 4),
+    (1234, 0),
+    (91212129, 9),
+    ])
+def test_next(circ_number, total):
+    assert sum_matching_neighbours(circ_number) == total
 
 
 if __name__ == '__main__':
