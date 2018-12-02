ef62d0cca3f9f28cef6891b87da7b3d9e0ade953
Rob Wells <rob@robjwells.com>
2018-12-02 19:36:59 +0000

    2018_02.py: Add stub file

---
+++ 2018/python/2018_02.py
@@ -0,0 +1,12 @@
+"""Advent of Code 2018 Day 2: Inventory Management System"""
+
+import aoc_common
+import pytest
+
+DAY = 2
+
+
+
+
+if __name__ == '__main__':
+    puzzle_input = aoc_common.load_puzzle_input(DAY)
