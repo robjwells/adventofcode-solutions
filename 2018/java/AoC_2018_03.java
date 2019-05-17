0c50887f3fd98bbebcd4d29260db68d452224581
Rob Wells <rob@robjwells.com>
2019-05-17 10:42:01 +0100

    2018_03.java: Add test for overlap matrix

--- 2018/java/AoC_2018_03.java
+++ 2018/java/AoC_2018_03.java
@@ -1,3 +1,4 @@
+import java.util.Arrays;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -8,7 +9,15 @@ class AoC_2018_03 extends Solution {
     public static void main(String[] args) {
         // Tests
         Test_2018_03.testClaimParses();
+        Test_2018_03.testOverlapMatrix();
     }
+
+    static int[][] makeOverlapMatrix(String[] claims, int sideLength) {
+        int[][] overlapMatrix = new int[sideLength][sideLength];
+        return overlapMatrix;
+    }
+
+}
 
 class Claim {
     private static Pattern claimPattern = Pattern.compile(
@@ -53,7 +62,30 @@ class Test_2018_03 {
         assert claim.height == 4;
     }
 
+    static void testOverlapMatrix() {
+        String[] claims = new String[] {
+            "#1 @ 1,3: 4x4",
+            "#2 @ 3,1: 4x4",
+            "#3 @ 5,5: 2x2",
+        };
+        int sideLength = 8;
+        int[][] result = AoC_2018_03.makeOverlapMatrix(claims, sideLength);
 
+        int[][] expected = new int[][] {
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 2, 2, 0, 0, 0},
+            {0, 0, 0, 2, 2, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+            {0, 0, 0, 0, 0, 0, 0, 0},
+        };
+
+        assert result.length == sideLength : "Number of rows incorrect";
+        for (int[] row : result) {
+            assert row.length == sideLength : "Number of columns incorrect.";
         }
+        assert Arrays.deepEquals(expected, result) : "Matrices unequal.";
     }
 }
