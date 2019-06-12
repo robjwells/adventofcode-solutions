c619bf2fda6fc56f523deb0f1ef66e9335865929
Rob Wells <rob@robjwells.com>
2019-06-12 23:08:14 +0100

    2016_01.java: Refactor and solve part two

--- 2016/java/AoC_2016_01.java
+++ 2016/java/AoC_2016_01.java
@@ -1,8 +1,10 @@
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.HashMap;
 import java.util.List;
-import java.util.function.Consumer;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import org.junit.Test;
@@ -24,6 +26,10 @@ public class AoC_2016_01 extends Solution {
         int partOneSolution = solvePartOne(instructions);
         assert 307 == partOneSolution : "solvePartOne() has failed regression test";
         System.out.printf("Part one: %s\n", partOneSolution);
+
+        int partTwoSolution = solvePartTwo(instructions);
+        assert 165 == partTwoSolution : "solvePartTwo() has failed regression test";
+        System.out.printf("Part two: %s\n", partTwoSolution);
     }
 
     static int solvePartOne(List<Instruction> instructions) {
@@ -32,27 +38,48 @@ public class AoC_2016_01 extends Solution {
         return position.distanceFromOrigin();
     }
 
+    static int solvePartTwo(List<Instruction> instructions) {
+        LoggingActor position = new LoggingActor();
+        try {
+            instructions.forEach(position::move);
+        } catch (LoggingActor.VisitedTwice exc) {
+            return position.distanceFromOrigin();
+        }
+        return 0;
+    }
+
     private static class Actor {
         private int x = 0;
         private int y = 0;
 
-        private int currentDirectionFunctionIndex = 0;
-
-        final List<Consumer<Integer>> functions = List.of(
-            (steps) -> y += steps,
-            (steps) -> x += steps,
-            (steps) -> y -= steps,
-            (steps) -> x -= steps
-        );
+        Direction currentDirection = Direction.NORTH;
 
         void move(Instruction instruction) {
-            int newIndex = Math.floorMod(
-                (currentDirectionFunctionIndex + instruction.getDirection().getIndexDelta()),
-                functions.size()
-            );
-            currentDirectionFunctionIndex = newIndex;
-            functions.get(newIndex).accept(instruction.getBlocks());
+            currentDirection = currentDirection.turn(instruction.getTurn());
+            move(instruction.getBlocks());
         }
+
+        void move(int steps) {
+            switch (currentDirection) {
+            case NORTH:
+                moveNorth(steps);
+                break;
+            case EAST:
+                moveEast(steps);
+                break;
+            case SOUTH:
+                moveSouth(steps);
+                break;
+            case WEST:
+                moveWest(steps);
+                break;
+            }
+        }
+
+        void moveNorth  (int steps) { y += steps; }
+        void moveEast   (int steps) { x += steps; }
+        void moveSouth  (int steps) { y -= steps; }
+        void moveWest   (int steps) { x -= steps; }
 
         int distanceFrom(int otherX, int otherY) {
             return Math.abs(x - otherX) + Math.abs(y - otherY);
@@ -62,6 +89,35 @@ public class AoC_2016_01 extends Solution {
             return distanceFrom(0, 0);
         }
 
+        @Override
+        public String toString() {
+            return String.format("(%d, %d)", x, y);
+        }
+
+    }
+
+    static class LoggingActor extends Actor {
+        private ArrayList<String> visited = new ArrayList<>();
+
+        @Override
+        void move(Instruction instruction) {
+            currentDirection = currentDirection.turn(instruction.getTurn());
+            int steps = instruction.getBlocks();
+            while (steps > 0) {
+                move(1);
+                if (visited.contains(toString())) {
+                    throw new VisitedTwice(toString());
+                }
+                visited.add(toString());
+                steps--;
+            }
+        }
+
+        static class VisitedTwice extends RuntimeException {
+            VisitedTwice(String message) {
+                super(message);
+            }
+        };
     }
 
     private static class Instruction {
@@ -73,7 +129,7 @@ public class AoC_2016_01 extends Solution {
             this.blocks = Integer.parseInt(instructionString.substring(1));
         }
 
-        public Turn getDirection() {
+        public Turn getTurn() {
             return turn;
         }
 
@@ -101,6 +157,28 @@ public class AoC_2016_01 extends Solution {
             return indexDelta;
         }
     };
+
+    private enum Direction {
+        NORTH(0), EAST(1), SOUTH(2), WEST(3);
+
+        private static Map<Integer, Direction> map = new HashMap<>();
+        static {
+            map.put(0, NORTH);
+            map.put(1, EAST);
+            map.put(2, SOUTH);
+            map.put(3, WEST);
+        }
+
+        private final int directionIndex;
+
+        Direction(int directionIndex) {
+            this.directionIndex = directionIndex;
+        }
+
+        Direction turn(Turn t) {
+            return map.get(Math.floorMod((directionIndex + t.getIndexDelta()), 4));
+        }
+    }
 
     @Test
     public void testPartOneDistanceOne() {
@@ -136,5 +214,16 @@ public class AoC_2016_01 extends Solution {
         Actor position = new Actor();
         input.forEach(position::move);
         assertEquals(12, position.distanceFromOrigin());
+    }
+
+    @Test
+    public void testPartTwo() {
+        List<Instruction> input = List.of(
+            new Instruction("R8"),
+            new Instruction("R4"),
+            new Instruction("R4"),
+            new Instruction("R8")
+        );
+        assertEquals(4, solvePartTwo(input));
     }
 }
