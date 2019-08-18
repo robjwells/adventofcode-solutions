efb672613fc4fde66e673797dbcbca55f7399479
Rob Wells <rob@robjwells.com>
2019-08-18 18:40:31 +0100

    Add 2015 C# folder

---
+++ 2015/cs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace advent_of_code_2015_csharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
