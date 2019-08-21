29f956fc4759cdb394f46fd66623175ef12d43db
Rob Wells <rob@robjwells.com>
2019-08-21 22:32:10 +0100

    2015-C#: Add command line app

---
+++ 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using AdventOfCode2015.Core;
+
+namespace AdventOfCode2015
+{
+    class Program
+    {
+        static ISolution[] days = new ISolution[] {
+            new Day01()
+        };
+
+        static void Main(string[] args)
+        {
+            foreach (ISolution day in days)
+            {
+                Console.WriteLine("Day {0}, year {1}", day.Day, day.Year);
+            }
+        }
+    }
+}
