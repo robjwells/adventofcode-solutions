8422758ec28b702936e3b66aba4abaa1ecc9f2d6
Rob Wells <rob@robjwells.com>
2019-08-21 22:33:56 +0100

    2015 01 C#: Update test file, add trivial tests

--- 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Tests/TestDay01.cs
+++ 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Tests/TestDay01.cs
@@ -5,15 +5,21 @@ namespace Tests
 {
     public class TestDay01
     {
+        Day01 instance = new Day01();
+
         [SetUp]
         public void Setup()
         {
         }
 
         [Test]
-        public void UltimateAnswer_Is42()
-        {
-            Assert.AreEqual(Day01.UltimateAnswer(), 42);
+        public void Day01_Day_Is1() {
+            Assert.AreEqual(instance.Day, 1);
+        }
+
+        [Test]
+        public void Day01_Year_Is2015() {
+            Assert.AreEqual(instance.Year, 2015);
         }
     }
 }
