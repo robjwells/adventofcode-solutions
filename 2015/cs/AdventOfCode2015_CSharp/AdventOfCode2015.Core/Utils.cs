0218040c104687b2b70ee389ace2ff97614a263c
Rob Wells <rob@robjwells.com>
2019-08-22 23:22:44 +0100

    2015 C# Find input directory from current location

--- 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Core/Utils.cs
+++ 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Core/Utils.cs
@@ -1,14 +1,35 @@
 namespace AdventOfCode2015.Core
 {
+    using System;
+    using System.Collections.Generic;
     using System.IO;
+    using System.Linq;
 
     public static class Utils
     {
-        public static string LoadInput(string inputDirectory, int year, int day)
+        public static string LoadInput(int year, int day)
         {
+            string inputDirectory = GetInputDirectory(year);
             string inputFileName = string.Format("{0}-{1:00}.txt", year, day);
             return File.ReadAllText(
                 Path.Combine(inputDirectory, inputFileName)
+            );
+        }
+
+        // See: https://stackoverflow.com/a/25573832/1845155
+        public static string GetInputDirectory(int year)
+        {
+            string baseDirectory = AppDomain.CurrentDomain.BaseDirectory;
+            List<string> pathParts = baseDirectory.Split(Path.DirectorySeparatorChar).ToList();
+
+            // Note that this’ll cause problems if you have the year repeated
+            // as a directory in the path to the Advent of Code repo.
+            int yearDirectoryIndex = pathParts.FindIndex(part => part == year.ToString());
+
+            return Path.Combine(
+                Path.GetPathRoot(baseDirectory),
+                Path.Combine(pathParts.Take(yearDirectoryIndex + 1).ToArray()),
+                "input"
             );
         }
     }
