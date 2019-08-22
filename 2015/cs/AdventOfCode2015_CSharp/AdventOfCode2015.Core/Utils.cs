1a5e6cb321d66b4fd93d556864da5e5e7bf3012a
Rob Wells <rob@robjwells.com>
2019-08-22 22:06:32 +0100

    2015 C# Format Utils

--- 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Core/Utils.cs
+++ 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Core/Utils.cs
@@ -1,7 +1,9 @@
-namespace AdventOfCode2015.Core {
+namespace AdventOfCode2015.Core
+{
     using System.IO;
 
-    public static class Utils {
+    public static class Utils
+    {
         public static string LoadInput(string inputDirectory, int year, int day)
         {
             string inputFileName = string.Format("{0}-{1:00}.txt", year, day);
