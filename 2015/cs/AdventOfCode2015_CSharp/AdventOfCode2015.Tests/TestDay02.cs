47f6e00ec4694659106771c23803efc5df508408
Rob Wells <rob@robjwells.com>
2019-08-23 00:11:12 +0100

    2015 02 C# Test Present construction from string

---
+++ 2015/cs/AdventOfCode2015_CSharp/AdventOfCode2015.Tests/TestDay02.cs
@@ -0,0 +1,20 @@
+using NUnit.Framework;
+using AdventOfCode2015.Core;
+
+namespace Tests
+{
+    public class TestDay02
+    {
+        [TestCase("1x2x3", 1, 2, 3)]
+        [TestCase("0x0x0", 0, 0, 0)]
+        [TestCase("10x20x30", 10, 20, 30)]
+        public void Day02_Present_CanConstructFromString(string input, int length, int width, int height)
+        {
+            Present p = Present.FromString(input);
+            Assert.AreEqual(
+                expected: (length, width, height),
+                actual: (p.Length, p.Width, p.Height)
+            );
+        }
+    }
+}
