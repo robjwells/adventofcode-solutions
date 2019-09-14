81a798bda882d91dd83de395d640e3d91d8242d6
Rob Wells <rob@robjwells.com>
2019-09-14 15:16:25 +0100

    2015 Java Allow running of single day

--- 2015/java/src/main/java/com/robjwells/adventofcode2015/App.java
+++ 2015/java/src/main/java/com/robjwells/adventofcode2015/App.java
@@ -3,15 +3,28 @@
  */
 package com.robjwells.adventofcode2015;
 
+import java.util.List;
+
 public class App {
-    private final static Solution[] days = {
-        new Day01()
-    };
+    private final static List<Solution> days = List.of(
+            new Day01()
+    );
+
+    private static void runOne(Solution day) {
+        String input = Utils.loadPuzzleInput(day.getYear(), day.getDay());
+        System.out.println(day.run(input));
+    }
+
+    private static void runAll() {
+        days.forEach(App::runOne);
+    }
 
     public static void main(String[] args) {
-        for (Solution day : days) {
-            String input = Utils.loadPuzzleInput(day.getYear(), day.getDay());
-            System.out.println(day.run(input) + "\n");
+        if (args.length == 0) {
+            runAll();
+        } else if (args.length == 1) {
+            int day = Integer.parseInt(args[0]) - 1;
+            runOne(days.get(day));
         }
     }
 }
