29721cba83578aefba1b4b345947b6ec95865f79
Rob Wells <rob@robjwells.com>
2019-09-14 12:25:32 +0100

    2015 01 Py Make more functional

--- 2015/python/2015-01.py
+++ 2015/python/2015-01.py
@@ -1,67 +1,67 @@
 #!/usr/bin/env python3
 """Advent of Code 2015, Day 1: Not Quite Lisp"""
+from itertools import accumulate
 
 import pytest
 
 
-def final_floor(instructions):
-    """Calculate the final floor for a given set of instructions
-
-    Instructions are a list of parentheses:
-        (   Move up a floor
-        )   Move down a floor
-    """
-    instruction_values = {'(': 1, ')': -1}
-    result = sum(instruction_values[char] for char in instructions)
-    return result
+def parse_input(instructions):
+    """Parse the parentheses into floor-change deltas (1, -1)."""
+    instruction_values = {"(": 1, ")": -1}
+    return [instruction_values[char] for char in instructions]
 
 
-def first_basement_char(instructions):
+def final_floor(deltas):
+    """Calculate the final floor for a given set of instructions."""
+    return sum(deltas)
+
+
+def first_basement_char(deltas):
     """Return position of char that puts Santa in the basement
 
     Positions start from 1.
     """
-    instruction_values = {'(': 1, ')': -1}
-    floor = 0
-    for position, char in enumerate(instructions, start=1):
-        floor += instruction_values[char]
-        if floor == -1:
-            return position
+    return next(
+        position
+        for (position, floor) in enumerate(accumulate(deltas), start=1)
+        if floor == -1
+    )
 
 
 def main(puzzle_input):
-    p1_final_floor = final_floor(puzzle_input)
-    print(f'Part one, final floor: {p1_final_floor}')
-    basement_instruction = first_basement_char(puzzle_input)
-    print(f'Part two, first basement instruction: {basement_instruction}')
+    parsed = parse_input(puzzle_input)
+    p1_final_floor = final_floor(parsed)
+    print(f"Part one, final floor: {p1_final_floor}")
+    basement_instruction = first_basement_char(parsed)
+    print(f"Part two, first basement instruction: {basement_instruction}")
 
 
-@pytest.mark.parametrize('instructions,expected', [
-    ('(())', 0),
-    ('()()', 0),
-    ('(((', 3),
-    ('(()(()(', 3),
-    ('))(((((', 3),
-    ('())', -1),
-    ('))(', -1),
-    (')))', -3),
-    (')())())', -3),
-    ])
+@pytest.mark.parametrize(
+    "instructions,expected",
+    [
+        ("(())", 0),
+        ("()()", 0),
+        ("(((", 3),
+        ("(()(()(", 3),
+        ("))(((((", 3),
+        ("())", -1),
+        ("))(", -1),
+        (")))", -3),
+        (")())())", -3),
+    ],
+)
 def test_final_floor(instructions, expected):
     """final_floor calculates correct final floor from instructions"""
-    assert final_floor(instructions) == expected
+    assert final_floor(parse_input(instructions)) == expected
 
 
-@pytest.mark.parametrize('instructions,expected', [
-    (')', 1),
-    ('()())', 5),
-    ])
+@pytest.mark.parametrize("instructions,expected", [(")", 1), ("()())", 5)])
 def test_first_basement_level(instructions, expected):
     """first_basement_char returns position of first sub-zero instruction"""
-    assert first_basement_char(instructions) == expected
+    assert first_basement_char(parse_input(instructions)) == expected
 
 
-if __name__ == '__main__':
-    with open('../input/2015-01.txt') as input_file:
+if __name__ == "__main__":
+    with open("../input/2015-01.txt") as input_file:
         puzzle_input = input_file.read().rstrip()
     main(puzzle_input)
