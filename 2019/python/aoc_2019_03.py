ed564239145ffb583692736b228b1dcde54021dc
Rob Wells <rob@robjwells.com>
2019-12-03 07:04:41 +0000

    2019 03 Python: Add part two (signal delay) test

--- 2019/python/aoc_2019_03.py
+++ 2019/python/aoc_2019_03.py
@@ -37,6 +37,36 @@ def test_find_closest_intersection_distance(
     assert find_closest_intersection_distance(wire_traces) == expected_distance
 
 
+@pytest.mark.parametrize(
+    "puzzle_input,expected_delay",
+    [
+        (("R8,U5,L5,D3" "\n" "U7,R6,D4,L4"), 30),
+        (
+            (
+                "R75,D30,R83,U83,L12,D49,R71,U7,L72"
+                "\n"
+                "U62,R66,U55,R34,D71,R55,D58,R83"
+            ),
+            610,
+        ),
+        (
+            (
+                "R98,U47,R26,D63,R33,U87,L62,D20,R33,U53,R51"
+                "\n"
+                "U98,R91,D20,R16,D67,R40,U7,R15,U6,R7"
+            ),
+            410,
+        ),
+    ],
+)
+def test_find_lowest_intersection_signal_delay(
+    puzzle_input: str, expected_delay: int
+) -> None:
+    parsed = parse_input(puzzle_input)
+    wire_traces = trace_wires(parsed)
+    assert find_lowest_intersection_signal_delay(wire_traces) == expected_delay
+
+
 class Instruction(NamedTuple):
     direction: str
     distance: int
