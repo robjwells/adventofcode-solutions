791ea8d2a29af944a7c5e872c992d4ada2060187
Rob Wells <rob@robjwells.com>
2019-12-06 21:24:56 +0000

    2019 05 Python: Implement parameter modes in Intcode

--- 2019/python/intcode.py
+++ 2019/python/intcode.py
@@ -1,9 +1,13 @@
+from aoc_common import split_number_by_places
 from collections import deque
+from itertools import zip_longest
 from operator import add, mul
-from typing import Callable, Dict, List, NamedTuple
+from typing import Callable, Dict, List, NamedTuple, Tuple
 
 PC_INCREMENT = 4
 HALT = 99
+
+ParameterModeList = List[int]
 
 
 class Instruction(NamedTuple):
@@ -25,6 +29,7 @@ class IntCode:
     _PC: int
     _memory: List[int]
     _instructions: Dict[int, Instruction]
+    _param_modes: Dict[int, Callable[[int], int]]
     input_queue: deque
     output_queue: deque
 
@@ -44,22 +49,36 @@ class IntCode:
             99: Instruction(99, 1, False, halt_execution),
         }
 
+        self._param_modes = {0: self._load, 1: lambda immediate: immediate}
+
     def _store(self, value: int, address: int) -> None:
         self._memory[address] = value
 
     def _load(self, address: int) -> int:
         return self._memory[address]
 
-    def opcode_to_function(self, opcode: int) -> Instruction:
-        return self._instructions[opcode]
+    def parse_opcode(self, full_opcode: int) -> Tuple[Instruction, ParameterModeList]:
+        modes, opcode = divmod(full_opcode, 100)
+        # Reverse the mode list as the modes are given in reverse order
+        # in the 'full' opcode.
+        mode_list = list(reversed(split_number_by_places(modes)))
+        return mode_list, self._instructions[opcode]
+
+    def load_parameters(self, parameters: List[int], modes: ParameterModeList):
+        return [
+            self._param_modes[mode](param)
+            for param, mode in zip_longest(parameters, modes, fillvalue=0)
+        ]
 
     def step(self) -> None:
         opcode = self._memory[self._PC]
-        _, length, store_result, action = self._instructions[opcode]
+        parameter_modes, instruction = self.parse_opcode(opcode)
+        _, length, store_result, action = instruction
         args = self._memory[self._PC + 1 : self._PC + length]
         if store_result:
-            loaded_args, destination = map(self._load, args[:-1]), args[-1]
-            result = action(*loaded_args)
+            parameters = self.load_parameters(args[:-1], parameter_modes)
+            destination = args[-1]
+            result = action(*parameters)
             self._store(result, destination)
         else:
             action(*args)
