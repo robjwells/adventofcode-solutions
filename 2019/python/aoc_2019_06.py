fbd7c23cdd9044bcff96371b015d75cba72922f1
Rob Wells <rob@robjwells.com>
2019-12-08 15:14:25 +0000

    2019 06 Python: Solve part two

--- 2019/python/aoc_2019_06.py
+++ 2019/python/aoc_2019_06.py
@@ -1,12 +1,14 @@
 """Day 6: Universal Orbit Map"""
+import math
 from collections import defaultdict, deque
-from typing import Dict, Iterator, List, Optional, NamedTuple, Tuple
+from copy import deepcopy
+from typing import Deque, DefaultDict, Iterator, List, NamedTuple, Tuple
 
 import aoc_common
 
 DAY = 6
 
-OrbitGraph = Dict[str, List[str]]
+OrbitGraph = DefaultDict[str, List[str]]
 
 
 class Orbit(NamedTuple):
@@ -19,13 +21,14 @@ def parse_input(input_text: str) -> List[Orbit]:
 
 
 def create_orbit_graph(orbits: List[Orbit]) -> OrbitGraph:
-    orbit_graph: Dict[str, List[str]] = defaultdict(list)
+    orbit_graph: DefaultDict[str, List[str]] = defaultdict(list)
     for orbited, orbited_by in orbits:
         orbit_graph[orbited].append(orbited_by)
     return orbit_graph
 
 
 def orbit_depths(orbit_graph: OrbitGraph) -> Iterator[int]:
+    """Yields node depths in a breadth-first traversal of the orbit graph."""
     queue = deque([(0, "COM")])
     while queue:
         depth, body = queue.popleft()
@@ -35,14 +38,76 @@ def orbit_depths(orbit_graph: OrbitGraph) -> Iterator[int]:
         )
 
 
-def main(orbit_list: List[Orbit]) -> Tuple[int, Optional[int]]:
+def find_shortest_path(
+    directed_orbit_graph: OrbitGraph, source: str = "YOU", dest: str = "SAN"
+) -> Tuple[int, List[str]]:
+    orbits = directed_to_undirected_graph(directed_orbit_graph)
+
+    source_body = orbits[source][0]
+    SearchEntry = Tuple[int, str, List[str]]
+    queue: Deque[SearchEntry] = deque(
+        [
+            # Depth, current node, path from source
+            (0, source_body, [])
+        ]
+    )
+
+    min_distance = math.inf
+    shortest_path: List[str] = []
+    while queue:
+        depth, body, path = queue.popleft()
+        if depth >= min_distance:
+            # Cut off search branch if distance is already too long.
+            continue
+
+        # Filter already-visited nodes from next steps to avoid cycles.
+        unvisited_neighbours = [n for n in orbits[body] if n not in path]
+
+        current_path = path + [body]
+        if dest in unvisited_neighbours:
+            # The earlier check ensures the current distance is known to be
+            # shorter than the previous-shortest, so we can just assign the
+            # current distance and path without testing again.
+            min_distance = depth
+            shortest_path = current_path
+        else:
+            queue.extend(
+                [
+                    (depth + 1, neighbour, current_path)
+                    for neighbour in unvisited_neighbours
+                ]
+            )
+
+    if min_distance is math.inf:
+        raise ValueError(f"Node {dest} not present in orbit graph.")
+
+    # Turn off mypy checking for the return value because the use of math.inf
+    # (which is a float) earlier causes it to complain about the return type
+    # really being a Union[float, int], where for any valid input graph the
+    # dest node will be found and min_distance will be an int.
+    return min_distance, shortest_path  # type: ignore
+
+
+def directed_to_undirected_graph(directed: OrbitGraph) -> OrbitGraph:
+    """Create undirected graph from the given directed graph."""
+    # Make an undirected graph so that we can traverse orbits in
+    # either direction. The original orbit graph is strictly
+    # orbited_body -> orbiting body, ie a directed acyclic graph.
+    undirected = deepcopy(directed)
+    for orbited_body, orbiting_bodies in directed.items():
+        for orbiting in orbiting_bodies:
+            undirected[orbiting].append(orbited_body)
+    return undirected
+
+
+def main(orbit_list: List[Orbit]) -> Tuple[int, int]:
     orbit_graph = create_orbit_graph(orbit_list)
     part_one_solution = sum(orbit_depths(orbit_graph))
+    part_two_solution, shortest_path = find_shortest_path(orbit_graph)
+    return (part_one_solution, part_two_solution)
 
-    return (part_one_solution, None)
 
-
-def test_orbit_depths():
+def test_orbit_depths() -> None:
     orbits = """\
 COM)B
 B)C
@@ -57,6 +122,27 @@ J)K
 K)L"""
     orbit_graph = create_orbit_graph(parse_input(orbits))
     assert sum(orbit_depths(orbit_graph)) == 42
+
+
+def test_find_shortest_path() -> None:
+    orbits = """\
+COM)B
+B)C
+C)D
+D)E
+E)F
+B)G
+G)H
+D)I
+E)J
+J)K
+K)L
+K)YOU
+I)SAN"""
+    orbit_graph = create_orbit_graph(parse_input(orbits))
+    distance, path = find_shortest_path(orbit_graph)
+    assert distance == 4
+    assert path == ["K", "J", "E", "D", "I"]
 
 
 if __name__ == "__main__":
