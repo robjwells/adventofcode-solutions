8d94982322702f2db59d631ed8218e3cabf6029e
Rob Wells <rob@robjwells.com>
2019-12-09 11:06:05 +0000

    2019 08 Python: Add SpaceImage class with test

---
+++ 2019/python/aoc_2019_08.py
@@ -0,0 +1,58 @@
+"""Day 8: Space Image Format"""
+from typing import Iterator, List
+
+import aoc_common
+
+DAY = 8
+
+
+class SpaceImage:
+    width: int
+    height: int
+    layers_count: int
+    _data: List[int]
+
+    def __init__(self, width: int, height: int, image_data: str) -> None:
+        self.width = width
+        self.height = height
+        self._data = self._data_string_to_digit_list(image_data)
+        layers_count = len(self._data) / (width * height)
+        assert layers_count.is_integer(), (
+            "Image data contains an incomplete number of layers"
+            f" ({layers_count}) for the given width ({width})"
+            f" and height ({height})."
+        )
+        self.layers_count = int(layers_count)
+
+    @staticmethod
+    def _data_string_to_digit_list(data: str) -> List[int]:
+        return [int(character) for character in data.strip()]
+
+    def __str__(self) -> str:
+        return "SpaceImage(width: {w}, height: {h}, layers: {lc})".format(
+            w=self.width, h=self.height, lc=self.layers_count
+        )
+
+    def __iter__(self) -> Iterator[List[int]]:
+        pixels_per_layer = self.width * self.height
+        for layer_index in range(self.layers_count):
+            layer_start = pixels_per_layer * layer_index
+            yield self._data[layer_start : layer_start + pixels_per_layer]
+
+
+def test_SpaceImage() -> None:
+    width, height = (3, 2)
+    data = "123456789012"
+    image = SpaceImage(width, height, data)
+
+    expected_layers = [[1, 2, 3, 4, 5, 6], [7, 8, 9, 0, 1, 2]]
+    assert list(image) == expected_layers
+
+
+def main() -> None:
+    pass
+
+
+if __name__ == "__main__":
+    image_data = aoc_common.load_puzzle_input(DAY)
+    image = SpaceImage(width=25, height=6, image_data=image_data)
