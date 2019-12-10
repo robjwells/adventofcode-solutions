5fe1385b1d66ca4b40656797c6cf77a474e8b5ec
Rob Wells <rob@robjwells.com>
2019-12-10 12:36:58 +0000

    2019 Python: Add tests for IntCode large memory and offset addressing

--- 2019/python/test_intcode.py
+++ 2019/python/test_intcode.py
@@ -114,3 +114,24 @@ def test_larger_comparison(program_input: int, expected_output: int) -> None:
     computer.pass_input(program_input)
     computer.run_until_halt()
     assert computer.read_output() == expected_output
+
+
+def test_offset_addressing_and_large_memory_reproduce_input() -> None:
+    program = [109, 1, 204, -1, 1001, 100, 1, 100, 1008, 100, 16, 101, 1006, 101, 0, 99]
+    computer = IntCode(program)
+    computer.run_until_halt()
+    assert list(computer.output_queue) == program
+
+
+def test_output_16_digit_number() -> None:
+    program = [1102, 34915192, 34915192, 7, 4, 7, 99, 0]
+    computer = IntCode(program)
+    computer.run_until_halt()
+    assert len(str(computer.read_output())) == 16
+
+
+def test_output_large_number() -> None:
+    program = [104, 1125899906842624, 99]
+    computer = IntCode(program)
+    computer.run_until_halt()
+    assert computer.read_output() == program[1]
