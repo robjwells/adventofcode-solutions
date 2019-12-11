33a3c63277c9b805e35b1aeacd3bb2457feb3eba
Rob Wells <rob@robjwells.com>
2019-12-11 19:12:26 +0000

    2019 11 Python: Refactor and tidy up

--- 2019/python/aoc_2019_11.py
+++ 2019/python/aoc_2019_11.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 
 from collections import defaultdict
 from enum import Enum
-from typing import DefaultDict, NamedTuple
+from typing import DefaultDict, List, NamedTuple, Tuple
 
 import aoc_common
 from intcode import HaltExecution, IntCode, parse_program
@@ -14,6 +14,10 @@ DAY = 11
 class Point(NamedTuple):
     x: int
     y: int
+
+    @classmethod
+    def origin(cls) -> Point:
+        return cls(0, 0)
 
 
 class Colour(Enum):
@@ -49,19 +53,19 @@ class Direction(Enum):
 Grid = DefaultDict[Point, Colour]
 
 
-def new_grid(default_colour: Colour = Colour.BLACK) -> Grid:
-    return defaultdict(lambda: default_colour)
+def create_black_grid() -> Grid:
+    return defaultdict(lambda: Colour.BLACK)
 
 
-def new_grid_with_white_starting_square() -> Grid:
-    grid = new_grid()
-    grid[Point(0, 0)] = Colour.WHITE
+def create_black_grid_with_one_white_panel() -> Grid:
+    grid = create_black_grid()
+    grid[Point.origin()] = Colour.WHITE
     return grid
 
 
 class HullPaintingRobot:
-    position: Point = Point(0, 0)
-    direction = Direction
+    position: Point = Point.origin()
+    direction: Direction
     grid: Grid
     computer: IntCode
 
@@ -72,7 +76,7 @@ class HullPaintingRobot:
         self.grid = grid
         self.computer = computer
 
-    def step(self):
+    def step(self) -> None:
         if self.computer.has_halted():
             return
 
@@ -88,7 +92,7 @@ class HullPaintingRobot:
         self.direction = self.direction.turn(turn)
         self.position = self.move(self.position, self.direction)
 
-    def run_until_halt(self):
+    def run_until_halt(self) -> None:
         try:
             while True:
                 self.step()
@@ -96,36 +100,69 @@ class HullPaintingRobot:
             pass
 
     @staticmethod
-    def move(position: Point, direction: Direction):
+    def move(position: Point, direction: Direction) -> Point:
         return Point(position.x + direction.value.x, position.y + direction.value.y)
 
 
-def main(program):
-    robot = HullPaintingRobot(grid=new_grid(), computer=IntCode(program))
+def paint_hull(program: List[int], grid: Grid) -> Grid:
+    robot = HullPaintingRobot(grid=grid, computer=IntCode(program))
     robot.run_until_halt()
-    print(len(robot.grid))  # 1967
+    return robot.grid
 
-    second_robot = HullPaintingRobot(
-        grid=new_grid_with_white_starting_square(), computer=IntCode(program)
-    )
-    second_robot.run_until_halt()
-    grid = second_robot.grid
+
+def visualise_painted_hull(grid: Grid) -> str:
     white_panels = [point for point, colour in grid.items() if colour is Colour.WHITE]
     white_panels = sorted(white_panels, key=lambda p: (-p.x, p.y))
     min_x, max_x = white_panels[-1].x, white_panels[0].x
     min_y = min(p.y for p in white_panels)
     max_y = max(p.y for p in white_panels)
 
+    hull = []
     for y in range(max_y, min_y - 1, -1):
+        line = ""
         for x in range(min_x, max_x + 1):
             if grid[Point(x, y)] is Colour.WHITE:
-                print("█", end="")
+                line += "█"
             else:
-                print(" ", end="")
-        print()
-    # KBUEGZBK
+                line += " "
+        hull.append(line.rstrip() + "\n")
+    hull_string = "".join(hull)
+    return hull_string
+
+
+def main(program: List[int]) -> Tuple[int, str]:
+    part_one_grid = paint_hull(program, create_black_grid())
+    unique_panels_painted = len(part_one_grid)
+
+    part_two_grid = paint_hull(program, create_black_grid_with_one_white_panel())
+    registration_id = visualise_painted_hull(part_two_grid)
+
+    return unique_panels_painted, registration_id
 
 
 if __name__ == "__main__":
     program = parse_program(aoc_common.load_puzzle_input(DAY))
-    main(program)
+    part_one_solution, part_two_solution = main(program)
+
+    assert (
+        part_one_solution == 1967
+    ), "Part one solution doesn't match known-correct answer."
+
+    part_two_expected = """\
+█  █ ███  █  █ ████  ██  ████ ███  █  █
+█ █  █  █ █  █ █    █  █    █ █  █ █ █
+██   ███  █  █ ███  █      █  ███  ██
+█ █  █  █ █  █ █    █ ██  █   █  █ █ █
+█ █  █  █ █  █ █    █  █ █    █  █ █ █
+█  █ ███   ██  ████  ███ ████ ███  █  █
+"""
+    assert (
+        part_two_solution == part_two_expected
+    ), "Part two solution doesn't match known-correct answer."
+
+    aoc_common.report_solution(
+        puzzle_title=__doc__,
+        part_one_solution=part_one_solution,
+        # Append newlines to clear "Part two solution:" line
+        part_two_solution="\n\n" + part_two_solution,
+    )
