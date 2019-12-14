89e6c041ac606db2bfab48c25718008cf77d128d
Rob Wells <rob@robjwells.com>
2019-12-14 15:23:36 +0000

    2019 12 Python: Solve part one

--- 2019/python/aoc_2019_12.py
+++ 2019/python/aoc_2019_12.py
@@ -2,9 +2,10 @@
 from __future__ import annotations
 
 import re
+from collections import defaultdict
 from functools import reduce
 from itertools import combinations
-from typing import Iterable, List, NamedTuple, Tuple
+from typing import DefaultDict, Iterable, List, NamedTuple, Tuple
 
 import aoc_common
 
@@ -44,10 +45,7 @@ class Moon(NamedTuple):
             Moon._single_gravity_delta(a, b) for a, b in zip(a.position, b.position)
         ]
         deltas_by_moon = [Velocity(*deltas) for deltas in zip(*velocity_deltas)]
-        new_velocities = map(
-            Velocity.combine, zip([a.velocity, b.velocity], deltas_by_moon)
-        )
-        return (next(new_velocities), next(new_velocities))
+        return deltas_by_moon[0], deltas_by_moon[1]
 
     @staticmethod
     def _single_gravity_delta(a: int, b: int) -> Tuple[int, int]:
@@ -68,10 +66,41 @@ class Moon(NamedTuple):
             self.velocity,
         )
 
+    @property
+    def potential_energy(self) -> int:
+        return sum(map(abs, self.position))  # type: ignore # something odd with abs
 
-# def simulate_step(moons=List[Moon]) -> List[Moon]:
-#     pairings = combinations(moons, 2)
-#     # for moon_a, moon_b in pairings:
+    @property
+    def kinetic_energy(self) -> int:
+        return sum(map(abs, self.velocity))  # type: ignore # something odd with abs
+
+    @property
+    def total_energy(self) -> int:
+        return self.potential_energy * self.kinetic_energy
+
+    def __str__(self) -> str:
+        px, py, pz = self.position
+        vx, vy, vz = self.velocity
+        return f"pos=<x={px: 3}, y={py: 3}, z={pz: 3}>, vel=<x={vx: 3}, y={vy: 3}, z={vz: 3}>"
+
+
+def simulate_step(moons: List[Moon]) -> List[Moon]:
+    pairings = combinations(moons, 2)
+    gravity_changes: DefaultDict[Moon, List[Velocity]] = defaultdict(list)
+    for moon_a, moon_b in pairings:
+        a_delta, b_delta = Moon.compute_gravity(moon_a, moon_b)
+        gravity_changes[moon_a].append(a_delta)
+        gravity_changes[moon_b].append(b_delta)
+    return [
+        moon.update_velocity(Velocity.combine(deltas)).apply_velocity()
+        for moon, deltas in gravity_changes.items()
+    ]
+
+
+def simulate_n_steps(moons: List[Moon], n: int = 1000) -> List[Moon]:
+    for _ in range(n):
+        moons = simulate_step(moons)
+    return moons
 
 
 def parse_input(input_string: str) -> List[Moon]:
@@ -84,5 +113,29 @@ def parse_input(input_string: str) -> List[Moon]:
     return [Moon(pos, Velocity.static()) for pos in positions]
 
 
+def main(moons: List[Moon]) -> int:
+    after_1000_steps = simulate_n_steps(moons, 1000)
+    all_moons_total_energy = sum(moon.total_energy for moon in after_1000_steps)
+
+    return all_moons_total_energy
+
+
 if __name__ == "__main__":
+    #     sample = """\
+    # <x=-1, y=0, z=2>
+    # <x=2, y=-10, z=-7>
+    # <x=4, y=-8, z=8>
+    # <x=3, y=5, z=-1>
+    # """
+    # moons = parse_input(sample)
+
     moons = parse_input(aoc_common.load_puzzle_input(DAY))
+    part_one_solution = main(moons)
+
+    assert (
+        part_one_solution == 12053
+    ), "Part one solution doesn't match known-correct answer."
+
+    aoc_common.report_solution(
+        puzzle_title=__doc__, part_one_solution=part_one_solution
+    )
