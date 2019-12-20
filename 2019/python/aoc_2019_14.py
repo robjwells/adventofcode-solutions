b04db1b36302598749ba87e44b307e35acb356c4
Rob Wells <rob@robjwells.com>
2019-12-20 07:45:21 +0000

    2019 14 Python: Improve typing

--- 2019/python/aoc_2019_14.py
+++ 2019/python/aoc_2019_14.py
@@ -2,7 +2,7 @@
 from __future__ import annotations
 
 from collections import deque
-from typing import Dict, Iterator, List, Tuple
+from typing import Deque, Dict, Iterator, List, Set, Tuple
 
 import aoc_common
 
@@ -24,13 +24,13 @@ def parse_input(data: str) -> Iterator[Reaction]:
         yield (output, reqs)
 
 
-def topo_dfs(graph: Dict[str, List[str]]):
-    L = deque()
+def topo_dfs(graph: Dict[str, List[str]]) -> Deque[str]:
+    L: Deque[str] = deque()
     unvisited = set(graph)
-    temporary_mark = set()
-    permanent_mark = set()
+    temporary_mark: Set[str] = set()
+    permanent_mark: Set[str] = set()
 
-    def visit(n: str):
+    def visit(n: str) -> None:
         if n in permanent_mark:
             return
         if n in temporary_mark:
@@ -48,7 +48,7 @@ def topo_dfs(graph: Dict[str, List[str]]):
     return L
 
 
-def main(reaction_gen) -> None:
+def main(reaction_gen: Iterator[Reaction]) -> None:
     reactions = list(reaction_gen)
     names = {chem[0]: [r[0] for r in reqs] for chem, reqs in reactions}
     ts = topo_dfs(names)
