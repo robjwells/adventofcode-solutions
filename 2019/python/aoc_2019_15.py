e503fd3eebcdd23ca307d9944d503622e1c626e4
Rob Wells <rob@robjwells.com>
2019-12-20 23:09:04 +0000

    2019 15 Python: Add skeleton file

---
+++ 2019/python/aoc_2019_15.py
@@ -0,0 +1,16 @@
+"""Day 15: Oxygen System"""
+
+from typing import List
+
+import aoc_common
+from intcode import IntCode, parse_program
+
+DAY = 15
+
+
+def main(program: List[int]) -> None:
+    pass
+
+
+if __name__ == "__main__":
+    main(aoc_common.load_puzzle_input(DAY))
