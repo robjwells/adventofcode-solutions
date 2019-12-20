c673e518305f1c63189f3175833d749af5763504
Rob Wells <rob@robjwells.com>
2019-12-20 09:01:59 +0000

    2019 14 Python: Solve part one

--- 2019/python/aoc_2019_14.py
+++ 2019/python/aoc_2019_14.py
@@ -1,8 +1,9 @@
 """Day 14: Space Stoichiometry"""
 from __future__ import annotations
 
-from collections import deque
-from typing import Deque, Dict, Iterator, List, Set, Tuple
+from collections import defaultdict, deque
+from math import ceil
+from typing import DefaultDict, Deque, Dict, Iterator, List, Set, Tuple
 
 import aoc_common
 
@@ -10,6 +11,7 @@ DAY = 14
 
 Chemical = Tuple[str, int]
 Reaction = Tuple[Chemical, List[Chemical]]
+Reactions = Dict[Chemical, List[Chemical]]
 
 
 def parse_input(data: str) -> Iterator[Reaction]:
@@ -48,11 +50,42 @@ def topological_sort(graph: Dict[str, List[str]]) -> Deque[str]:
     return L
 
 
-def main(reaction: List[Reaction]) -> None:
-    names = {chem[0]: [r[0] for r in reqs] for chem, reqs in reactions}
-    ts = topological_sort(names)
+def order_reactions(reactions: Reactions) -> Reactions:
+    names = {chem[0]: [r[0] for r in reqs] for chem, reqs in reactions.items()}
+    dependency_order = topological_sort(names)
+    # Dictionaries are kept in insertion order from 3.6
+    return dict(
+        sorted(reactions.items(), key=lambda r: dependency_order.index(r[0][0]))
+    )
+
+
+def ore_requirements(reactions: Reactions, fuel_needed: int = 1) -> int:
+    reactions = order_reactions(reactions)
+    amounts: DefaultDict[str, int] = defaultdict(lambda: 0)
+    amounts["FUEL"] = fuel_needed
+    for product, precursors in reactions.items():
+        product_name, product_batch_amount = product
+        product_needed = amounts[product_name]
+        batches_needed = ceil(product_needed / product_batch_amount)
+        for precursor_name, precursor_per_batch in precursors:
+            total_precursor_needed = batches_needed * precursor_per_batch
+            amounts[precursor_name] += total_precursor_needed
+    return amounts["ORE"]
+
+
+def main(reactions: Reactions) -> int:
+    minimum_ore_for_one_fuel = ore_requirements(reactions)
+    return minimum_ore_for_one_fuel
 
 
 if __name__ == "__main__":
-    reactions = list(parse_input(aoc_common.load_puzzle_input(DAY)))
-    main(reactions)
+    reactions = dict(parse_input(aoc_common.load_puzzle_input(DAY)))
+    part_one_solution = main(reactions)
+
+    assert (
+        part_one_solution == 261960
+    ), "Part one solution doesn't match known-correct answer."
+
+    aoc_common.report_solution(
+        puzzle_title=__doc__, part_one_solution=part_one_solution
+    )
