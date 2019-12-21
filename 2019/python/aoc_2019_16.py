3c39ce85b9abdb1438b9af01cc6bb430a5791265
Rob Wells <rob@robjwells.com>
2019-12-21 22:41:37 +0000

    2019 16 Python: Solve part one (relatively slow)

--- 2019/python/aoc_2019_16.py
+++ 2019/python/aoc_2019_16.py
@@ -37,9 +37,46 @@ def test_pattern(place: int, first_eight_digits: List[int]) -> None:
     )
 
 
-def main(signal: List[int]) -> None:
-    pass
+def fft(signal: List[int]) -> List[int]:
+    output: List[int] = []
+    for place in range(1, len(signal) + 1):
+        sum_of_products = sum(
+            signal_digit * pattern_digit
+            for signal_digit, pattern_digit in zip(signal, pattern(place))
+            if pattern_digit
+        )
+        new_digit = abs(sum_of_products) % 10
+        output.append(new_digit)
+    return output
+
+
+def repeat_fft(signal: List[int], phases: int) -> List[int]:
+    for _ in range(phases):
+        signal = fft(signal)
+    return signal
+
+
+def rejoin_place_list(digits: List[int]) -> int:
+    rejoined = 0
+    for p10, digit in enumerate(reversed(digits)):
+        rejoined += digit * 10 ** p10
+    return rejoined
+
+
+def main(signal: List[int]) -> int:
+    after_100_phases = repeat_fft(signal, phases=100)
+    after_100_first_eight = rejoin_place_list(after_100_phases[:8])
+    return after_100_first_eight
 
 
 if __name__ == "__main__":
     signal = aoc_common.split_number_by_places(int(aoc_common.load_puzzle_input(DAY)))
+    part_one_solution = main(signal)
+
+    assert (
+        part_one_solution == 76795888
+    ), "Part one solution doesn't match known-correct answer."
+
+    aoc_common.report_solution(
+        puzzle_title=__doc__, part_one_solution=part_one_solution
+    )
