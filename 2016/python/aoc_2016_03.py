e0b3b767ccb7fc601eb7b40d336f94d75f8aa43c
Rob Wells <rob@robjwells.com>
2020-12-31 13:38:40 +0000

    2016-03.py: Sort triples in separate step

--- 2016/python/aoc_2016_03.py
+++ 2016/python/aoc_2016_03.py
@@ -6,18 +6,20 @@ from aoc_common import load_puzzle_input, report_solution
 
 
 def parse_horizontal(string: str) -> List[Tuple[int, int, int]]:
-    """Parse the instruction lines into sorted triples of side lengths."""
-    sorted_sides = [
-        sorted(int(x) for x in line.split()) for line in string.splitlines()
-    ]
-    triples = [(sides[0], sides[1], sides[2]) for sides in sorted_sides]
-    return triples
+    """Parse the instruction lines into triples of side lengths."""
+    sides = [[int(x) for x in line.split()] for line in string.splitlines()]
+    return [(s[0], s[1], s[2]) for s in sides]
 
 
 def filter_valid_triangles(
     triples: List[Tuple[int, int, int]]
 ) -> List[Tuple[int, int, int]]:
+    triples = sort_sides(triples)
     return [triple for triple in triples if triple[0] + triple[1] > triple[2]]
+
+
+def sort_sides(triples: List[Tuple[int, int, int]]) -> List[Tuple[int, int, int]]:
+    return [(t[0], t[1], t[2]) for t in [sorted(sides) for sides in triples]]
 
 
 if __name__ == "__main__":
