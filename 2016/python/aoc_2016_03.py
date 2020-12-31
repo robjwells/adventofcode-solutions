203dc112494fbac90f35838a00a5f27a92674ddf
Rob Wells <rob@robjwells.com>
2020-12-31 13:39:40 +0000

    2016-03.py: Introduce type alias for triple

--- 2016/python/aoc_2016_03.py
+++ 2016/python/aoc_2016_03.py
@@ -1,31 +1,32 @@
 from __future__ import annotations
 
-from more_itertools import chunked
 from typing import List, Tuple
+
+from more_itertools import chunked
 
 from aoc_common import load_puzzle_input, report_solution
 
+Triple = Tuple[int, int, int]
 
-def parse_horizontal(string: str) -> List[Tuple[int, int, int]]:
+
+def parse_horizontal(string: str) -> List[Triple]:
     """Parse the instruction lines into triples of side lengths."""
     sides = [[int(x) for x in line.split()] for line in string.splitlines()]
     return [(s[0], s[1], s[2]) for s in sides]
 
 
-def filter_valid_triangles(
-    triples: List[Tuple[int, int, int]]
-) -> List[Tuple[int, int, int]]:
+def filter_valid_triangles(triples: List[Triple]) -> List[Triple]:
     triples = sort_sides(triples)
     return [triple for triple in triples if triple[0] + triple[1] > triple[2]]
 
 
-def parse_vertical(string: str) -> List[Tuple[int, int, int]]:
+def parse_vertical(string: str) -> List[Triple]:
     horizontal_triples = parse_horizontal(string)
     all_vertical = [s[col] for col in range(3) for s in horizontal_triples]
     return [(s[0], s[1], s[2]) for s in chunked(all_vertical, 3)]
 
 
-def sort_sides(triples: List[Tuple[int, int, int]]) -> List[Tuple[int, int, int]]:
+def sort_sides(triples: List[Triple]) -> List[Triple]:
     return [(t[0], t[1], t[2]) for t in [sorted(sides) for sides in triples]]
 
 
