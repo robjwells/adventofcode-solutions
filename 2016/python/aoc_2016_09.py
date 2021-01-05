5e6d5d2d198b07d72fc924e930961e9f6b1967f7
Rob Wells <rob@robjwells.com>
2021-01-05 12:53:16 +0000

    2016 09 Python: Solve part two

--- 2016/python/aoc_2016_09.py
+++ 2016/python/aoc_2016_09.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from enum import auto, Enum
 import re
 
 import pytest
@@ -9,20 +10,43 @@ from aoc_common import load_puzzle_input, report_solution
 _marker = re.compile(r"\( (?P<length> \d+ ) x (?P<times> \d+ ) \)", flags=re.VERBOSE)
 
 
-def decompressed_length(string: str, position: int = 0, length: int = 0) -> int:
+class FormatVersion(Enum):
+    ONE = auto()
+    TWO = auto()
+
+
+def decompressed_length(
+    string: str,
+    format_version: FormatVersion = FormatVersion.ONE,
+    *,
+    position: int = 0,
+    length: int = 0,
+) -> int:
     if position >= len(string):
         return length
     if match := _marker.match(string, pos=position):
         repeat_length = int(match["length"])
         repeat_times = int(match["times"])
-        new_length = length + (repeat_length * repeat_times)
-        new_position = match.end(0) + repeat_length
-        return decompressed_length(string, new_position, new_length)
-    return decompressed_length(string, position + 1, length + 1)
+        next_position = match.end(0) + repeat_length
+        if format_version is FormatVersion.ONE:
+            new_length = length + (repeat_length * repeat_times)
+        elif format_version is FormatVersion.TWO:
+            recursive_length = decompressed_length(
+                string[match.end(0) : next_position], format_version
+            )
+            new_length = length + (recursive_length * repeat_times)
+        else:
+            raise ValueError(f"Unknown format version: {format_version}")
+        return decompressed_length(
+            string, format_version, position=next_position, length=new_length
+        )
+    return decompressed_length(
+        string, format_version, position=position + 1, length=length + 1
+    )
 
 
 @pytest.mark.parametrize(
-    "string,expected",
+    "string,expected_length",
     [
         ("ADVENT", 6),
         ("A(1x5)BC", 7),
@@ -32,13 +56,29 @@ def decompressed_length(string: str, position: int = 0, length: int = 0) -> int:
         ("X(8x2)(3x3)ABCY", 18),
     ],
 )
-def test_decompressed_length(string: str, expected: int) -> None:
-    assert decompressed_length(string) == expected
+def test_decompressed_length(string: str, expected_length: int) -> None:
+    assert decompressed_length(string) == expected_length
+
+
+@pytest.mark.parametrize(
+    "string,expected_length",
+    [
+        ("(3x3)XYZ", 9),
+        ("X(8x2)(3x3)ABCY", 20),
+        ("(27x12)(20x12)(13x14)(7x10)(1x12)A", 241920),
+        ("(25x3)(3x3)ABC(2x3)XY(5x2)PQRSTX(18x9)(3x2)TWO(5x7)SEVEN", 445),
+    ],
+)
+def test_v2_decompressed_length(string: str, expected_length: int) -> None:
+    assert decompressed_length(string, FormatVersion.TWO) == expected_length
 
 
 if __name__ == "__main__":
     compressed = load_puzzle_input(day=9)
     v1_length = decompressed_length(compressed)
+    v2_length = decompressed_length(compressed, FormatVersion.TWO)
     report_solution(
-        puzzle_title="Day 9: Explosives in Cyberspace", part_one_solution=v1_length
+        puzzle_title="Day 9: Explosives in Cyberspace",
+        part_one_solution=v1_length,
+        part_two_solution=v2_length,
     )
