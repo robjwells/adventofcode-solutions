69e90a76674415e83b759f16fd8867339217afd6
Rob Wells <rob@robjwells.com>
2021-01-23 09:30:33 +0000

    2016 11 Python: Work on state classes

--- 2016/python/aoc_2016_11.py
+++ 2016/python/aoc_2016_11.py
@@ -2,16 +2,16 @@ from __future__ import annotations
 
 from dataclasses import dataclass
 from enum import Enum, auto
-from typing import Any, List, Tuple, cast
+from typing import Any, Iterable, NamedTuple, cast
 
 from lark import Lark, Transformer
-from pyrsistent import pvector
+from more_itertools import chunked
+from pyrsistent import pvector, v
 from pyrsistent.typing import PVector
 
-from aoc_common import load_puzzle_input
+import pytest
 
-with open("aoc_2016_11.lark") as grammar_file:
-    grammar = grammar_file.read()
+from aoc_common import load_puzzle_input
 
 
 class ItemKind(Enum):
@@ -20,6 +20,9 @@ class ItemKind(Enum):
 
     def __repr__(self) -> str:
         return self.name.lower()  # pylint: disable=no-member
+
+    def __lt__(self, other: ItemKind) -> bool:
+        return self is ItemKind.GENERATOR
 
 
 @dataclass(frozen=True)
@@ -37,7 +40,110 @@ class Floor:
     items: PVector[Item]
 
     def __str__(self) -> str:
-        return f"{self.number}: {list(self.items)}"
+        return f"Floor {self.number}: {list(self.items)}"
+
+    @property
+    def is_valid(self) -> bool:
+        """True when there are no generators or each chip has its generator."""
+        gen_elements = {i.element for i in self.items if i.kind is ItemKind.GENERATOR}
+        chip_elements = {i.element for i in self.items if i.kind is ItemKind.MICROCHIP}
+        return not gen_elements or chip_elements <= gen_elements
+
+
+@pytest.mark.parametrize(
+    "floor,expected",
+    [
+        # Empty floor is valid.
+        (Floor(0, v()), True),
+        # Floors with only one item are valid.
+        (Floor(0, v(Item(element="a", kind=ItemKind.GENERATOR))), True),
+        (Floor(0, v(Item(element="a", kind=ItemKind.MICROCHIP))), True),
+        # A floor with a matching pair is valid.
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.GENERATOR),
+                    Item(element="a", kind=ItemKind.MICROCHIP),
+                ),
+            ),
+            True,
+        ),
+        # A floor with a differing pair is invalid.
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.GENERATOR),
+                    Item(element="b", kind=ItemKind.MICROCHIP),
+                ),
+            ),
+            False,
+        ),
+        # A floor with a matched pair is valid even
+        # if there are other generators present.
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.GENERATOR),
+                    Item(element="b", kind=ItemKind.GENERATOR),
+                    Item(element="b", kind=ItemKind.MICROCHIP),
+                ),
+            ),
+            True,
+        ),
+        # A floor with an unmatched chip is invalid if there
+        # are (matched or unmatched) generators present.
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.GENERATOR),
+                    Item(element="a", kind=ItemKind.MICROCHIP),
+                    Item(element="b", kind=ItemKind.MICROCHIP),
+                ),
+            ),
+            False,
+        ),
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.GENERATOR),
+                    Item(element="b", kind=ItemKind.MICROCHIP),
+                    Item(element="c", kind=ItemKind.MICROCHIP),
+                ),
+            ),
+            False,
+        ),
+        # Floors with only one kind of item are valid.
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.MICROCHIP),
+                    Item(element="b", kind=ItemKind.MICROCHIP),
+                    Item(element="c", kind=ItemKind.MICROCHIP),
+                ),
+            ),
+            True,
+        ),
+        (
+            Floor(
+                0,
+                v(
+                    Item(element="a", kind=ItemKind.GENERATOR),
+                    Item(element="b", kind=ItemKind.GENERATOR),
+                    Item(element="c", kind=ItemKind.GENERATOR),
+                ),
+            ),
+            True,
+        ),
+    ],
+)
+def test_floor_validity(floor: Floor, expected: bool) -> None:
+    assert floor.is_valid == expected
 
 
 class _T(Transformer[Floor]):
@@ -69,6 +175,9 @@ class _T(Transformer[Floor]):
         return Floor(floor, pvector(items))
 
 
+with open("aoc_2016_11.lark") as grammar_file:
+    grammar = grammar_file.read()
+
 _parse = Lark(grammar).parse
 _transform = _T().transform
 
@@ -77,6 +186,55 @@ def parse(line: str) -> Floor:
     return _transform(_parse(line))
 
 
+class LocationPair(NamedTuple):
+    generator: int
+    microchip: int
+
+
+@dataclass(frozen=True)
+class MinimumState:
+    elevator_floor: int
+    pairs: PVector[LocationPair]
+
+    @classmethod
+    def from_iterable(
+        cls, elevator_floor: int, iterable: Iterable[LocationPair]
+    ) -> MinimumState:
+        return cls(elevator_floor, pvector(sorted(iterable)))
+
+
+@dataclass(frozen=True)
+class State:
+    elevator_floor: int
+    floors: PVector[Floor]
+
+    def _group(self) -> Iterable[LocationPair]:
+        nested = [
+            [(floor.number, item) for item in floor.items] for floor in self.floors
+        ]
+        flat = [pair for floor in nested for pair in floor]
+        s = sorted(flat, key=lambda pair: (pair[1].element, pair[1].kind))
+        for generator, chip in chunked(s, 2):
+            gen_floor, _ = generator
+            chip_floor, _ = chip
+            yield LocationPair(gen_floor, chip_floor)
+
+    @property
+    def is_valid(self) -> bool:
+        ...
+
+    def to_minimum_state(self) -> MinimumState:
+        return MinimumState.from_iterable(self.elevator_floor, self._group())
+
+
 if __name__ == "__main__":
     lines = load_puzzle_input(day=11).splitlines()
-    print(*[parse(line) for line in lines], sep="\n\n")
+    s = State(0, pvector([parse(line) for line in lines]))
+    s1 = State(0, pvector([parse(line) for line in lines]))
+    s2 = State(1, pvector([parse(line) for line in lines]))
+    seen = {s.to_minimum_state()}
+    print(seen)
+    seen.add(s1.to_minimum_state())
+    print(seen)
+    seen.add(s2.to_minimum_state())
+    print(seen)
