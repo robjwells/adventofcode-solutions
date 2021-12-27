ed26619c92ffb6cbf1c4647f6ec4e928409508e0
Rob Wells <rob@robjwells.com>
2021-12-27 19:34:12 +0000

    2017 Python: Update for new aoc.py module, black

--- 2017/python/2017-01.py
+++ 2017/python/2017-01.py
@@ -1,5 +1,7 @@
 #!/usr/bin/env python3
 
+import aoc
+
 import pytest
 
 
@@ -69,54 +71,62 @@ def total_matching_neighbours(circular_number):
 def total_half_distant(circular_number):
     """Sums all digits that match the digit halfway distant in the sequence"""
     return total_nearby_digits_in_sequence(
-        circular_number,
-        digit_distance=len(str(circular_number)) // 2)
+        circular_number, digit_distance=len(str(circular_number)) // 2
+    )
 
 
-@pytest.mark.parametrize('number,digit_distance,pairs', [
-    (451,  1, [(4, 5), (5, 1), (1, 4)]),
-    (1122, 1, [(1, 1), (1, 2), (2, 2), (2, 1)]),
-    (1122, 2, [(1, 2), (1, 2), (2, 1), (2, 1)]),
-    (1212, 1, [(1, 2), (2, 1), (1, 2), (2, 1)]),
-    (1212, 2, [(1, 1), (2, 2), (1, 1), (2, 2)]),
-    (123123, 1, [(1, 2), (2, 3), (3, 1), (1, 2), (2, 3), (3, 1)]),
-    (123123, 3, [(1, 1), (2, 2), (3, 3), (1, 1), (2, 2), (3, 3)]),
-    ])
+@pytest.mark.parametrize(
+    "number,digit_distance,pairs",
+    [
+        (451, 1, [(4, 5), (5, 1), (1, 4)]),
+        (1122, 1, [(1, 1), (1, 2), (2, 2), (2, 1)]),
+        (1122, 2, [(1, 2), (1, 2), (2, 1), (2, 1)]),
+        (1212, 1, [(1, 2), (2, 1), (1, 2), (2, 1)]),
+        (1212, 2, [(1, 1), (2, 2), (1, 1), (2, 2)]),
+        (123123, 1, [(1, 2), (2, 3), (3, 1), (1, 2), (2, 3), (3, 1)]),
+        (123123, 3, [(1, 1), (2, 2), (3, 3), (1, 1), (2, 2), (3, 3)]),
+    ],
+)
 def test_circ_pairs(number, digit_distance, pairs):
     """circ_pairs pairs digits in number with digit specified distance away"""
     sequence = int_to_sequence(number)
     assert list(circ_pairs(sequence, distance=digit_distance)) == pairs
 
 
-@pytest.mark.parametrize('circ_number,total', [
-    (1122, 3),
-    (1111, 4),
-    (1234, 0),
-    (91212129, 9),
-    ])
+@pytest.mark.parametrize(
+    "circ_number,total",
+    [
+        (1122, 3),
+        (1111, 4),
+        (1234, 0),
+        (91212129, 9),
+    ],
+)
 def test_next(circ_number, total):
     """Test total_matching_neighbours against known input and output"""
     assert total_matching_neighbours(circ_number) == total
 
 
-@pytest.mark.parametrize('circ_number,total', [
-    (1212, 6),
-    (1221, 0),
-    (123425, 4),
-    (123123, 12),
-    (12131415, 4),
-    ])
+@pytest.mark.parametrize(
+    "circ_number,total",
+    [
+        (1212, 6),
+        (1221, 0),
+        (123425, 4),
+        (123123, 12),
+        (12131415, 4),
+    ],
+)
 def test_half_distant(circ_number, total):
     """Test total_half_distant against known input and output"""
     assert total_half_distant(circ_number) == total
 
 
 def main(puzzle_input):
-    print('Part one:', total_matching_neighbours(puzzle_input))
-    print('Part two:', total_half_distant(puzzle_input))
+    print("Part one:", total_matching_neighbours(puzzle_input))
+    print("Part two:", total_half_distant(puzzle_input))
 
 
-if __name__ == '__main__':
-    with open('../input/2017-01.txt') as f:
-        puzzle_input = int(f.read())
+if __name__ == "__main__":
+    puzzle_input = int(aoc.load_puzzle_input(2017, 1))
     main(puzzle_input)
